932bbab33e8f10d1cf49d920db5252c0380c71b8
cesar-lima <cesarr340@gmail.com>
2025-02-23 19:09:03 -0300

    remover ano da página inicial

--- web/src/app/(home)/page.tsx
+++ web/src/app/(home)/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 				<Image src={logo} alt="devstage" width={108.5} height={30} />
 
 				<h1 className="text-4xl - text-center leading-none font-heading font-medium flex flex-col md:text-7xl md:text-left">
-					<span className="text-blue">CodeCraft</span>Summit 2025
+					<span className="text-blue">CodeCraft</span>Summit
 				</h1>
 			</div>
 
